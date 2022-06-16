fee4cbdce14644b7cdf23e5e12ffd69975e729df
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-06-16 23:56:57 +0530

    Creating a product and passing it with the help of props

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Grid from "@material-ui/core";
-
+const products = [
+  { id: 1, name: "Shoes", description: "Running shoes" },
+  { id: 2, name: "Mackbook", description: "Apple mackbook" },
+];
 const Products = () => {
   <main>
-    <Grid container justify="center" spacing={4}></Grid>
+    <Grid container justify="center" spacing={4}>
+      {products.map((product) => (
+        <Grid item key={product.id} xs={12} sm={6} lg={3}>
+          <Product product={product} />
+        </Grid>
+      ))}
+    </Grid>
   </main>;
 };
 export default Products;
