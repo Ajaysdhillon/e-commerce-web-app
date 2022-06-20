b1a6df588c787e581fc43d5294c8600c4658a553
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-06-20 23:31:56 +0530

    Importing the component to app.js and adding image

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,18 +1,36 @@
 import React from "react";
-import Grid from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import Product from "./Product/Product";
+
 const products = [
-  { id: 1, name: "Shoes", description: "Running shoes", price: "$5" },
-  { id: 2, name: "Mackbook", description: "Apple mackbook", price: "$15" },
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Running shoes",
+    price: "$5",
+    image:
+      "https://in.images.search.yahoo.com/search/images;_ylt=AwrxysxbtbBiEDsA7hG7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=shoes&fr2=piv-web&type=E211IN1274G0&fr=mcafee#id=3&iurl=https%3A%2F%2Fcdn.sweatband.com%2Fhead_sprint_team_2.0_mens_tennis_shoes_head_sprint_team_2.0_mens_tennis_shoes_2000x2000.jpg&action=click",
+  },
+  {
+    id: 2,
+    name: "Mackbook",
+    description: "Apple mackbook",
+    price: "$15",
+    image:
+      "https://in.images.search.yahoo.com/search/images;_ylt=AwrxzAGCtbBirSwAUW.7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=macbbok&fr2=piv-web&type=E211IN1274G0&fr=mcafee#id=5&iurl=https%3A%2F%2Fi5.walmartimages.com%2Fasr%2Fd7dd917d-49d4-4c4a-a769-e81071720765.6fbb62b3b6a44fc4ab63066bb9dab8fe.jpeg&action=click",
+  },
 ];
 const Products = () => {
-  <main>
-    <Grid container justify="center" spacing={4}>
-      {products.map((product) => (
-        <Grid item key={product.id} xs={12} sm={6} lg={3}>
-          <Product product={product} />
-        </Grid>
-      ))}
-    </Grid>
-  </main>;
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
 };
 export default Products;
