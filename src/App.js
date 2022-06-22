46a169c6b027357af9f0de9ac7fd3b6187d84858
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-06-22 23:38:44 +0530

    Setting up commerce.js to act as backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
-
+import { commerce } from "./lib/commerce";
 import Products from "./components/Products/Products";
 function App() {
+  const [products, setProducts] = useState([]);
   return (
     <div>
       <Products />
