64376b95a1f87fcb76e9b9ed81ee6bcd934d99bd
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-06-25 20:17:20 +0530

    Adding the functionality of cart to add products

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import { commerce } from "./lib/commerce";
 import Products from "./components/Products/Products";
 function App() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
     setProducts(data);
   };
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
   return (
     <div>
