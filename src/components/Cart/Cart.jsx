5faf75f8a4c4ea1fe5f1c6ff27afcad9f7df373b
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-06-25 21:24:39 +0530

    Making commponent to view our cart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import { CallMissedSharp } from "@material-ui/icons";
+const Cart = ({ cart }) => {
+  const isEmpty = !cart.line_items.length;
+  const EmptyCart = () => {
+    <Typography variant="subtitle1">
+      You have no items in your shopping cart. Add some!!
+    </Typography>;
+  };
+
+  const FilledCart = () => {
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <div>{item.name}</div>
+          </Grid>
+        ))}
+      </Grid>
+    </>;
+  };
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3">
+        Your Shopping Cart
+      </Typography>
+      {isEmpty ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
+
+export default Cart;
