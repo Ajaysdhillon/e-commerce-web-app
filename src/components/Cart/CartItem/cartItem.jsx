4e8c17b2cee57cd83290770e0b52b9d37af4ce75
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-06-29 23:41:27 +0530

    Adding padding to heading of cart page

--- src/components/Cart/CartItem/cartItem.jsx
+++ src/components/Cart/CartItem/cartItem.jsx
@@ -1,5 +1,4 @@
-import { React } from "react";
-import useStyles from "./styles";
+import React from "react";
 import {
   Typography,
   Button,
@@ -9,38 +8,58 @@ import {
   CardMedia,
 } from "@material-ui/core";
 
-const cartItem = ({ item }) => {
+import useStyles from "./styles";
+
+const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
   const classes = useStyles();
 
+  const handleUpdateCartQty = (lineItemId, newQuantity) =>
+    onUpdateCartQty(lineItemId, newQuantity);
+
+  const handleRemoveFromCart = (lineItemId) => onRemoveFromCart(lineItemId);
+
   return (
-    <Card>
+    <Card className="cart-item">
       <CardMedia
         image={item.media.source}
         alt={item.name}
         className={classes.media}
       />
-      <CardContent>
+      <CardContent className={classes.cardContent}>
         <Typography variant="h4">{item.name}</Typography>
         <Typography variant="h5">
-          {item.line_total.formatted_wih_symbol}
+          {item.line_total.formatted_with_symbol}
         </Typography>
-        <CardActions className={classes.cardActions}>
-          <div className={classes.button}>
-            <Button type="button" size="small">
-              -
-            </Button>
-            <Typography>{item.quantity}</Typography>
-            <Button type="button" size="small">
-              +
-            </Button>
-          </div>
-          <Button variant="contained" type="button" color="secondary">
-            Remove
-          </Button>
-        </CardActions>
       </CardContent>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}>
+          <Button
+            type="button"
+            size="small"
+            onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}
+          >
+            -
+          </Button>
+          <Typography>&nbsp;{item.quantity}&nbsp;</Typography>
+          <Button
+            type="button"
+            size="small"
+            onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}
+          >
+            +
+          </Button>
+        </div>
+        <Button
+          variant="contained"
+          type="button"
+          color="secondary"
+          onClick={() => handleRemoveFromCart(item.id)}
+        >
+          Remove
+        </Button>
+      </CardActions>
     </Card>
   );
 };
 
-export default cartItem;
+export default CartItem;
