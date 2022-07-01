4c320c98e5f75e8b98ee996043b4c9a2c18f76d3
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-07-01 23:43:59 +0530

    Adding button to move in and out of the cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,13 +12,20 @@ import { ShoppingCart } from "@material-ui/icons";
 import { mergeClasses } from "@material-ui/styles";
 import logo from "../../assets/commerce.png";
 import useStyles from "./styles";
+import { Link } from "react-router-dom";
 const Navbar = ({ totalItems }) => {
   const classes = useStyles();
   return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
-          <Typography variant="h6" className={classes.title} color="inherit">
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
             <img
               src={logo}
               alt="Commerce.js"
@@ -29,7 +36,13 @@ const Navbar = ({ totalItems }) => {
           </Typography>
           <div className={classes.grow} />
           <div className={classes.button}>
-            <IconButton aria-label="Show cart items" color="inherit">
+            <Link to="/cart">Go to cart</Link>
+            <IconButton
+              component={Link}
+              to="/cart"
+              aria-label="Show cart items"
+              color="inherit"
+            >
               <Badge badgeContent={totalItems} color="secondary">
                 <ShoppingCart />
               </Badge>
