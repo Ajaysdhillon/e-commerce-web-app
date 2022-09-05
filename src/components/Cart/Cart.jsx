221953e355c484034ae3aeb9da997bf08469e501
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-09-05 23:11:09 +0530

    Removing cartItem error

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
-import CartItem from "./cartItem/cartItem";
+import CartItem from "./CartItem/cartItem";
 import useStyles from "./styles";
 
 const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
